c62175d3aadf2912efd150f428d75dd25004751d
niccoloruberti <nikiruberti@gmail.com>
2023-05-23 16:48:22 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //creo l'array di immagini
+
 let images = [
     './img/01.webp',
     './img/02.webp',
@@ -12,16 +13,43 @@ let images = [
 let slider = document.querySelector('.slider');
 
 //creo un ciclo che inserisce tutte le immagini dell'array nello slider
+
 for (let i = 0; i < images.length; i++) {
-    let courrentImage = images[i]
+    let currentImage = images[i]
     slider.innerHTML +=`<div class="slide">
-                            <img src="${courrentImage}">
+                            <img src="${currentImage}">
                         </div>`
 }
 
 //creo un HTML collection con tutte le slide
+
 let allSlides = document.getElementsByClassName('slide');
 
 //rendo visibile la prima slide
+
 let visibleElement = 0;
 allSlides[visibleElement].classList.add('visible');
+
+//creo l'evento per il teasto che fa scorrere le slide in avanti
+
+let buttonDown = document.querySelector('.down');
+
+buttonDown.addEventListener('click', function() {
+    if (visibleElement < images.length - 1) {
+        allSlides[visibleElement].classList.remove('visible');
+        visibleElement++;
+        allSlides[visibleElement].classList.add('visible');
+    }
+})
+
+//creo l'evento per il tasto che fa scorrere le slide indietro
+
+let buttonUp = document.querySelector('.up');
+
+buttonUp.addEventListener('click', function() {
+    if (visibleElement > 0) {
+        allSlides[visibleElement].classList.remove('visible');
+        visibleElement--;
+        allSlides[visibleElement].classList.add('visible');
+    }
+})
